ad87d52742759184dc565b537c27b9c5fac76240
mdaoud <mdaoud@aspire-infotech.net>
2019-07-09 15:20:13 +0300

    Adding "Editing bidder from bidders table" scenario to bidderModification file

--- cypress/integration/ui/bidderModification.js
+++ cypress/integration/ui/bidderModification.js
@@ -1,6 +1,7 @@
 const bidderName = Cypress.moment().format('YY.MM.DD_hh:mm:ss') + '-UI_automation-Bidder_created';
 const biddeUpdatedName = Cypress.moment().format('YY.MM.DD_hh:mm:ss') + '-UI_automation-Bidder_updated';
 
+
 context('Editing Bidder UI', () => {
   describe('Create/ Edit Bidder - UI', () => {
     beforeEach(() => {
@@ -35,11 +36,12 @@ context('Editing Bidder UI', () => {
       //cy.get('[data-qa="bidder--data-row--name"]').should('contain', bidderName);
       cy.get('[data-qa="bidder--data-row--name"]').first()
       .should('contain', bidderName) // // Confirming the new createed bidder is at the top of the table
-      .click({force: true});;  
+      .click({force: true});  
     });
 
-    it('Editing the Bidder values', () => {      
-      cy.get('[data-qa="bidder-detail--edit"]').wait(1000).click().then(() => {
+    it('Editing the Bidder values from "bidders" table', () => {      
+      cy.get('[data-qa="bidder--data-row--select--actions"]').first().click();
+      cy.get('[data-qa="bidder--data-row--edit"]').click().wait(1500).then(() => {
         cy.url().should('include', 'buyers-dashboard/bidders/edit'); // User redirected to bidder editing page
       });
       cy.get('[data-qa="bidder-add-edit--name-input"]').focus().clear().type(biddeUpdatedName, { force: true });
@@ -79,8 +81,51 @@ context('Editing Bidder UI', () => {
       cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(5) p3')
       .should('contain', 'http://google.com');
       cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(6) p3')
-      .should('contain', 'https://google.com');
-      
+      .should('contain', 'https://google.com');      
   });
+
+  it('Editing the Bidder values from specific bidder page', () => {      
+    cy.get('[data-qa="bidder-detail--edit"]').click().wait(1500).then(() => {
+      cy.url().should('include', 'buyers-dashboard/bidders/edit'); // User redirected to bidder editing page
+    });
+    cy.get('[data-qa="bidder-add-edit--name-input"]').focus().clear().type(biddeUpdatedName, { force: true });
+    cy.get('[data-qa="toggle-tabs--select-PMP"]').click({force: true});
+    cy.get('[data-qa="bidder-add-edit--non-secure-url-input"]').clear().type("http://kargo.com", { force: true });
+    cy.get('[data-qa="bidder-add-edit--secure-url-input"]').clear().type("https://kargo.com", { force: true });
+    cy.get('[data-qa="bidder-add-edit--send--non-matched"]').click({force: true});
+    cy.get('[data-qa="bidder-add-edit--send--iframe-wrapper"]').click({force: true});
+    cy.get('[data-qa="bidder-add-edit--send--gzip"]').click({force: true});  
+    cy.get('[data-qa="bidder-add-edit--send--cookie-sync"]').click({force: true});
+    cy.get('[data-qa="bidder-add-edit--send--video-traffic"]').click({force: true});
+    cy.get('[data-qa="bidder-add-edit--send--native-traffic"]').click({force: true});
+    cy.get('[data-qa="bidder-add-edit--send--sync-weight"]').clear().type("0.5", { force: true });
+    cy.get('[data-qa="bidder-add-edit--save"]').click({force: true});
+    cy.get('[data-qa="modal--confirm"]').click({force: true}).wait(1000);
+  });
+
+  it('Validating Bidder was updated from detail rail in UI', () => {
+    //After Bidder is successfully created, UI should navigate to bidder page
+    // Search functionality is not worknig for bidders page at the moment.
+    //  So, for now comments will be added to the "Search" functionality lines to prevent them from execution
+    /////////// > "Search" functionality lines  
+    //cy.get('[type="search"]').type(bidderName, { force: true });
+    //cy.get('[data-qa="bidder--data-row--name"]').should('contain', bidderName);
+    cy.get('[data-qa="bidder--data-row--name"]').first()
+    .should('contain', biddeUpdatedName).click({force: true}).wait(1500);
+    cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(2) p3')
+    .should('contain', 'PMP');
+    cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(3) p3')
+    .should('contain', '0.5');
+    cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(4) p3:nth-child(2)')
+    .should('contain', 'Send non-matched users');
+    cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(4) p3:nth-child(3)')
+    .should('contain', 'Use iframe wrapper for sync URL users');
+    cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(4) p3:nth-child(4)')
+    .should('contain', 'Send gzip request');
+    cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(5) p3')
+    .should('contain', 'http://kargo.com');
+    cy.get('section.u-grid.layout--details.u-grid-gap-24 aside div div:nth-child(6) p3')
+    .should('contain', 'https://kargo.com');      
+});
 });
 });
